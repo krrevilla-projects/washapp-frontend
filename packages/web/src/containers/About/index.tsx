d1014462c0f154a00a857534d6239d756c3e6c7a
Karl Revilla <karlangelorevilla@gmail.com>
2023-04-03 20:23:10 +0800

    Added react-navigation web

---
+++ packages/web/src/containers/About/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const AboutContainer = () => {
+  return (
+    <View>
+      <Text>About</Text>
+    </View>
+  );
+};
+
+export default AboutContainer;
