3288c4e8510797c2dc9d79b87bc493b24f75623c
Karl Revilla <karlangelorevilla@gmail.com>
2023-04-25 20:45:41 +0800

    Lost pixel (#7)
    * Initialized Lost Pixel
    
    * Initialized Lost Pixel
    
    * Updated lost-pixel-test.yml
    
    * Updated lostpixel config

---
+++ packages/mobile/lostpixel.config.ts
@@ -0,0 +1,8 @@
+import type {CustomProjectConfig} from 'lost-pixel';
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: './packages/mobile/storybook-static',
+  },
+  generateOnly: true,
+};
