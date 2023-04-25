e47b437f1017389a65c28455a4b1e64a21d0a43f
Karl Revilla <karlangelorevilla@gmail.com>
2023-04-25 21:50:33 +0800

    Updated lostpixel config (#8)
    * Updated lostpixel config
    
    * Updated lostpixel config

--- packages/mobile/lostpixel.config.ts
+++ packages/mobile/lostpixel.config.ts
@@ -4,5 +4,9 @@ export const config: CustomProjectConfig = {
   storybookShots: {
     storybookUrl: './packages/mobile/storybook-static',
   },
+  imagePathBaseline: './packages/mobile/baseline-images',
+  imagePathCurrent: './packages/mobile/current-images',
+  imagePathDifference: './packages/mobile/difference-images',
   generateOnly: true,
+  failOnDifference: true,
 };
